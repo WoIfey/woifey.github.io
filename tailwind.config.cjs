7595838b1e79dd180a7a6ee213515ed10fdce475
WoIfey <wuflerz@outlook.com>
2023-05-15 14:18:09 +0300

    Updates

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,5 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{js,ts,jsx,tsx,html}"],
+  content: ["./src/**/*.{html,js,jsx}"],
   theme: {
     extend: {
       fontFamily: {
